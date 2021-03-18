1e01b81d680023fd0be6a7af9d25c3657dccfb47
dingyanghao <1010768459@qq.com>
2021-03-19 00:07:06 +0800

    v-01

---
+++ src/main/java/com/example/slr/SlrApplication.java
@@ -0,0 +1,13 @@
+package com.example.slr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slr/SlrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
